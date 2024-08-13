49796a4f287040d358e54c648852811229b6a608
Leva_Ziaei <levaziaee3@gmail.com>
2024-08-13 23:50:12 +0330

    Update-Leva

--- src/main/java/com/beautysalon/beautysalonsystem/controller/servlet/CheckPaymentServlet.java
+++ src/main/java/com/beautysalon/beautysalonsystem/controller/servlet/CheckPaymentServlet.java
@@ -1,6 +1,5 @@
 package com.beautysalon.beautysalonsystem.controller.servlet;
 
-import com.beautysalon.beautysalonsystem.model.service.CashPaymentService;
 import com.beautysalon.beautysalonsystem.model.service.CheckPaymentService;
 import jakarta.inject.Inject;
 import jakarta.servlet.annotation.WebServlet;
