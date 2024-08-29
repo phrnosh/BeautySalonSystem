8e2a06cc20ca002c79ab9d3efe47ea9de30a0506
Farnoosh <farnoshgh20@gmail.com>
2024-08-29 23:50:24 +0330

    Salon-Manager

--- src/main/java/com/beautysalon/beautysalonsystem/controller/servlet/ManagerSalonServlet.java
+++ src/main/java/com/beautysalon/beautysalonsystem/controller/servlet/ManagerSalonServlet.java
@@ -1,29 +1,29 @@
-//package com.beautysalon.beautysalonsystem.controller.servlet;
-//
-//import com.beautysalon.beautysalonsystem.model.entity.Salon;
-//import com.beautysalon.beautysalonsystem.model.service.ManagerService;
-//import com.beautysalon.beautysalonsystem.model.service.SalonService;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import jakarta.inject.Inject;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.extern.slf4j.Slf4j;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//@Slf4j
-//@WebServlet("/manager/salon.do")
-//public class ManagerSalonServlet extends HttpServlet {
-//
-//    @Inject
-//    private ManagerService managerService;
-//
-//    @Inject
-//    private SalonService salonService;
-//
+package com.beautysalon.beautysalonsystem.controller.servlet;
 
-//}
+import com.beautysalon.beautysalonsystem.model.entity.Salon;
+import com.beautysalon.beautysalonsystem.model.service.ManagerService;
+import com.beautysalon.beautysalonsystem.model.service.SalonService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@WebServlet("/manager/salon.do")
+public class ManagerSalonServlet extends HttpServlet {
+
+    @Inject
+    private ManagerService managerService;
+
+    @Inject
+    private SalonService salonService;
+
+
+}
