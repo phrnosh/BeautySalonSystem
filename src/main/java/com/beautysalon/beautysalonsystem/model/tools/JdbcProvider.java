8f88718eca1c9be80d4d99af8531e2e00b2fcdbf
Leva_Ziaei <levaziaee3@gmail.com>
2024-09-13 11:11:58 +0330

    update

---
+++ src/main/java/com/beautysalon/beautysalonsystem/model/tools/JdbcProvider.java
@@ -0,0 +1,23 @@
+package com.mftplus.jee02.model.tools;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcProvider {
+    @Getter
+    private static JdbcProvider jdbcProvider = new JdbcProvider();
+
+    private JdbcProvider() {
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "javaee",
+                "java123"
+        );
+    }
+}
