3f4ae49d4b6ff91dfdf07c9a81430dc01bb4dbf9
Mahoor <mahurshams94@gmail.com>
2024-10-01 15:13:11 +0330

    Rest Class Added - Mahoor

---
+++ src/main/java/com/beautysalon/beautysalonsystem/controller/rest/Rest.java
@@ -0,0 +1,9 @@
+package com.beautysalon.beautysalonsystem.controller.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class Rest extends Application {
+
+}
